dfa49c6025bd3707673607ed8e6a9df6bc631a11
danihae <dani.hae@posteo.de>
2023-10-24 13:37:36 +0200

    Fix redundant exec of parse_measurement_data

--- compile.py
+++ compile.py
@@ -9,9 +9,6 @@ folder_data = 'X:/Lara/2023parakrineAndLiveStains/'
 # get measurement files (adjust if less subdirectories)
 mlf_files = glob.glob(folder_data + '*/*/*/MeasurementData.mlf')
 
-# parse measurement files and create data frame
-df_imgs, plates, wells, fields = parse_measurement_data(mlf_files)
-
 # folder to save compiled data
 folder_export = 'D:/Lara/compiled/paracrine/'
 os.makedirs(folder_export, exist_ok=True)
@@ -22,9 +19,12 @@ def compile_field_wrapper(plate, well, field, df_imgs, folder_export):
     compile_field(df_imgs, plate=plate, well=well, field=field, folder_export=folder_export)
 
 
-# Create a list of tuples containing the arguments for each iteration
-args_list = [(plate, well, field, df_imgs, folder_export) for plate, well, field in product(plates, wells, fields)]
-
 if __name__ == '__main__':
+    # parse measurement files and create data frame
+    df_imgs, plates, wells, fields = parse_measurement_data(mlf_files)
+
+    # Create a list of tuples containing the arguments for each iteration
+    args_list = [(plate, well, field, df_imgs, folder_export) for plate, well, field in product(plates, wells, fields)]
+
     with Pool(2) as pool:  # specify number of processes
         pool.starmap(compile_field_wrapper, args_list)
