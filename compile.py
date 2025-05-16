7afbf74f3cfef0f1d5ac82cb78a142e182d239cc
daniel <dani.hae@posteo.de>
2025-05-16 13:04:00 +0200

    Update compile.py paths and pool size configuration

--- compile.py
+++ compile.py
@@ -3,14 +3,14 @@ from functions import *
 from itertools import product
 from multiprocessing import Pool
 
-# data folder
-folder_data = 'X:/Lara/2023parakrineAndLiveStains/'
+# data folder with raw tif files
+folder_data = 'X:/path/of/directory/'
 
 # get measurement files (adjust if less subdirectories)
 mlf_files = glob.glob(folder_data + '*/*/*/MeasurementData.mlf')
 
 # folder to save compiled data
-folder_export = 'D:/Lara/compiled/paracrine/'
+folder_export = 'D:/path/output/'
 os.makedirs(folder_export, exist_ok=True)
 
 
@@ -18,6 +18,7 @@ os.makedirs(folder_export, exist_ok=True)
 def compile_field_wrapper(plate, well, field, df_imgs, folder_export):
     compile_field(df_imgs, plate=plate, well=well, field=field, folder_export=folder_export)
 
+n_pools = 4
 
 if __name__ == '__main__':
     # parse measurement files and create data frame
@@ -26,5 +27,5 @@ if __name__ == '__main__':
     # Create a list of tuples containing the arguments for each iteration
     args_list = [(plate, well, field, df_imgs, folder_export) for plate, well, field in product(plates, wells, fields)]
 
-    with Pool(2) as pool:  # specify number of processes
+    with Pool(n_pools) as pool:  # specify number of processes
         pool.starmap(compile_field_wrapper, args_list)
